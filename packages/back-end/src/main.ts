43c265c112152058a56f7fdca0a87f2aedce8a10
danny gonzalez <jesusgalicia2019@gmail.com>
2024-07-21 18:13:23 -0400

    faet: add mongoDb and mongoose

--- packages/back-end/src/main.ts
+++ packages/back-end/src/main.ts
@@ -1,8 +1,10 @@
 import { createServer } from 'http';
 import configuration from './config/configuration';
 import app from './app';
+import { connectDatabase } from './common/database';
 
 async function bootstrap() {
+  await connectDatabase();
   const server = createServer(app.callback());
   server.listen(configuration.PORT, () => {
     console.log(`server running on port :${configuration.PORT}`);
