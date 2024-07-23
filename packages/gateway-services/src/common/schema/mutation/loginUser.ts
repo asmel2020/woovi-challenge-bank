aba8283cf642287702566fe5cd277d173dd2c54d
danny gonzalez <jesusgalicia2019@gmail.com>
2024-07-23 00:27:13 -0400

    refactor: add control error not servicers connect

--- packages/gateway-services/src/common/schema/mutation/loginUser.ts
+++ packages/gateway-services/src/common/schema/mutation/loginUser.ts
@@ -2,8 +2,7 @@ import configuration from '../../../config/configuration';
 import { Args } from '../../interfaces';
 import { IUserLoginRespond, Respond } from '../interfaces/ILoginUserRespond.interfaces';
 import { gql, GraphQLClient, request } from 'graphql-request';
-export const loginUser = async ({ email, password }: Omit<Args, 'name'>): Promise<Respond> => {
-  
+export const loginUser = async ({ email, password }: Omit<Args, 'name'>) => {
   const mutation = gql`
          mutation UserLogin {
             userLogin(input: { email: "${email}", password: "${password}" }) {
@@ -15,14 +14,18 @@ export const loginUser = async ({ email, password }: Omit<Args, 'name'>): Promis
         }
     `;
 
-
   const graphQLClient = new GraphQLClient(`${configuration.AUTH_SERVICES.AUTH_SERVICES_URL}/graphql`, {
     headers: {
       authenticator: configuration.AUTH_SERVICES.AUTH_SERVICES_API_KEY,
     },
   });
 
-  const { userLogin }: IUserLoginRespond = await graphQLClient.request(mutation);
-
-  return userLogin;
+  try {
+    const { userLogin }: IUserLoginRespond = await graphQLClient.request(mutation);
+    return userLogin;
+  } catch (error) {
+    return {
+      error: 'Server not available',
+    };
+  }
 };
--- packages/gateway-services/src/common/schema/mutation/registerUser.ts
+++ packages/gateway-services/src/common/schema/mutation/registerUser.ts
@@ -1,9 +1,9 @@
 import configuration from '../../../config/configuration';
 import { Args } from '../../interfaces';
 import { IUserRegisterRespond, Respond } from '../interfaces/ILoginUserRespond.interfaces';
-import { gql, GraphQLClient} from 'graphql-request';
+import { gql, GraphQLClient } from 'graphql-request';
 
-export const registerUser = async ({ name, email, password }: Args): Promise<Respond> => {
+export const registerUser = async ({ name, email, password }: Args) => {
   const mutation = gql`
     mutation UserRegister {
         userRegister(input: { name: "${name}", email: "${email}", password: "${password}" }) {
@@ -13,14 +13,19 @@ export const registerUser = async ({ name, email, password }: Args): Promise<Res
         }
     }
   `;
-  
+
   const graphQLClient = new GraphQLClient(`${configuration.AUTH_SERVICES.AUTH_SERVICES_URL}/graphql`, {
     headers: {
       authenticator: configuration.AUTH_SERVICES.AUTH_SERVICES_API_KEY,
     },
   });
-  
-  const { userRegister }: IUserRegisterRespond = await graphQLClient.request(mutation);
 
-  return userRegister;
+  try {
+    const { userRegister }: IUserRegisterRespond = await graphQLClient.request(mutation);
+    return userRegister;
+  } catch (error) {
+    return {
+      error: 'Server not available',
+    };
+  }
 };
