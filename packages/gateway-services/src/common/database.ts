f41e97d338c556c23dacfa6914f9e8dee547439f
danny gonzalez <jesusgalicia2019@gmail.com>
2024-07-22 22:39:49 -0400

    refactor: remove mongoDB of gateway services

--- packages/gateway-services/src/common/database.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import configuration from '../config/configuration';
-
-export const connectDatabase = async (): Promise<void> => {
-  // eslint-disable-next-line no-console
-  mongoose.connection.on('close', () => console.log('Database connection closed.'));
-
-  await mongoose.connect(configuration.MONGO_URI);
-};
--- packages/gateway-services/src/common/helpers/generateTokenJWT.ts
+++
@@ -1,6 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-import configuration from '../../config/configuration';
-import { IUser } from '../../modules/auth/UserModel';
-
-export const generateTokenJWT = (user: IUser) => jwt.sign({ id: user._id }, configuration.JWT_SECRET);
