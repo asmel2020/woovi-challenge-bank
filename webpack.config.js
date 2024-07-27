98d1b9bce6bacdf61f1bdfb134d1a64898f99f23
danny gonzalez <jesusgalicia2019@gmail.com>
2024-07-27 18:49:04 -0400

    feat: add web pack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  target: 'node',
+  entry: './packages/server/src/main.ts', // Adjust the entry point as needed
+  output: {
+    path: path.resolve(__dirname, './packages/server/dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@bank/server': path.resolve(__dirname, 'packages/server/src')
+    },
+    fallback: {
+      crypto: false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: [/node_modules/, /dist/, /test/, /__tests__/]
+      }
+    ]
+  }
+};
